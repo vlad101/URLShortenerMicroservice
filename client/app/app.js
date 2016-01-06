6f969aeec8c991a967615334a3b4a5f4c5d8ba35
Vladimir Efros <vladimir.efros@gmail.com>
2016-01-06 14:47:18 +0000

    first commit

---
+++ client/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('workspaceApp', [
+  'workspaceApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('workspaceApp')
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+  });
